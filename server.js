59f010fe2684091fbf7bdb62211e43d42e2f6097
Thomas Kelly <tmkelly28@gmail.com>
2018-07-18 17:10:32 -0400

    update starting point

--- server.js
+++ server.js
@@ -31,17 +31,16 @@ const puppies = [{
   image: 'http://4.bp.blogspot.com/-3JeIxWBU7bY/UKjIt8lVpCI/AAAAAAAABx8/YM8piSOwczs/s1600/Schipperke-Puppy.jpg'
 }];
 
-app.get('/api/puppies', function (req, res) {
+app.get('/api/puppies', (req, res) => {
   res.json(puppies.map(({id, name}) => ({id, name})));
 });
 
-app.get('/api/puppies/:id', function (req, res) {
+app.get('/api/puppies/:id', (req, res) => {
   const aPuppy = puppies.find(p => p.id === Number(req.params.id));
   if (!aPuppy) res.status(404).end();
   else res.json(aPuppy);
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Server listening on port', 3000);
 });
-
